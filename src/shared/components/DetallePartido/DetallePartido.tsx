e727558427fe59fb224e2329e498f9b01b85caaa
luan <luantreia@gmail.com>
2026-01-25 23:45:15 -0300

    a

--- src/shared/components/DetallePartido/DetallePartido.tsx
+++ src/shared/components/DetallePartido/DetallePartido.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useEntity } from '../../hooks';
 import { PartidoService, type Partido } from '../../../features/partidos/services/partidoService';
