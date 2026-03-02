aecba2c5b352bfebfa1e4a066026818e0e37988b
luan <luantreia@gmail.com>
2026-03-02 06:51:04 -0300

    a

--- src/features/competencias/components/RankedEvolutionChartModal.tsx
+++ src/features/competencias/components/RankedEvolutionChartModal.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { RankedService } from "../services/rankedService";
 import {
